0c62ceb7234b8b7b44d70c937777b413e0497cdc
Justice Marks <her@shedaddy.com>
2019-07-25 21:11:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,10 @@
 # require modules here
 require "yaml"
 
-def load_library
+def load_library(path)
   # code goes here
-  
-  
+  result = YAML.load_file(path)
+  return result
 end
 
 def get_japanese_emoticon
